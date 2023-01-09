eeb9830c06e048626c70646dbe2592fb7f904df6
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2023-01-09 16:42:14 +0600

    0=>Individual Radio Button selected successfully2 Alhamdulillah...

--- lib/pages/question_screen.dart
+++ lib/pages/question_screen.dart
@@ -1,21 +1,7 @@
-import 'dart:collection';
-import 'dart:convert';
-import 'package:collection/collection.dart';
-
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:pora_lekha/controllers/question_controller.dart';
-//import 'package:pora_lekha/db/question-model/question_model.dart';
 import '../models/question_model2.dart';
-import 'package:pora_lekha/utils/dimensions/Dimensions.dart';
 import 'package:http/http.dart' as http;
-
-//import '../models/question_model.dart';
-
-//import '../db/question-model/question_model.dart';
-
-// List<String> groupValues = [];
 
 class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
@@ -25,60 +11,33 @@ class QuestionScreen extends StatefulWidget {
 }
 
 class _QuestionScreenState extends State<QuestionScreen> {
+  List<String> groupVal = [];
   QuestionController questionController = QuestionController();
-  //String groupValue = '';
-  List<String> groupValues = [];
-  List<String> answers = [];
-  //LinkedList<String> groupValuesLL = <String>[] as LinkedList<String>;
-  double totalMarks = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+    questionController.getAllQuestions().then((value) {
+      value.forEach((element) {
+        groupVal.add(element['question']);
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    //List<String> groupValues = [];
-    //List<String> answers = [];
-
     return Scaffold(
       appBar: AppBar(),
       body: FutureBuilder(
         initialData: const [],
         future: questionController.getAllQuestions(),
         builder: (context, snapshot) {
-          final groupValuesFixedLengthList =
-              List<String>.filled(snapshot.data!.length, 'fill');
-
-          final answersFixedLengthList =
-              List<String>.filled(snapshot.data!.length, 'results');
-          print(groupValuesFixedLengthList);
-          print(answersFixedLengthList);
-
-          groupValuesFixedLengthList[1] = 'ggg';
-          print(groupValuesFixedLengthList);
-
-          answers = [];
-          answers = answersFixedLengthList;
-          groupValues = [];
-          groupValues = groupValuesFixedLengthList;
-          //String groupValue = '';
           return ListView.builder(
-            itemCount: snapshot.data!.length, //5, //snapshot.data!.length,
+            itemCount: snapshot.data!.length,
             itemBuilder: (context, index) {
-              //print(groupValuesFixedLengthList);
-              /* 
-              groupValuesFixedLengthList[index] =
-                  snapshot.data![index]['question'];
-
-              answersFixedLengthList[index] = snapshot.data![index]['Answer']; */
-
-              //String groupValue = snapshot.data![index]['question'];
-              //groupValues.add(snapshot.data![index]['question']);
-
-              //answers.add(snapshot.data![index]['Answer']);
-              //groupValues.add(snapshot.data![index]['question']);
-
               return Column(
                 children: [
                   Container(
-                    //height: Dimensions.height20 * 30,
-                    // width: Dimensions.width20 * 30,
                     color: Colors.amber,
                     padding: const EdgeInsets.all(20),
                     child: Column(
@@ -92,32 +51,11 @@ class _QuestionScreenState extends State<QuestionScreen> {
                           title: Text(
                             snapshot.data![index]['A'],
                           ),
-                          value: 3, //snapshot.data![index]['A'],
-                          groupValue: groupValuesFixedLengthList[index], //3,
-                          /* groupValuesFixedLengthList[
-                              index],  */ //groupValues[index],
-                          /* snapshot.data![index]
-                              ['question'],  */ //groupValues[index],
+                          value: snapshot.data![index]['A'],
+                          groupValue: groupVal[index],
                           onChanged: (value) {
                             setState(() {
-                              //groupValues[index] = value.toString();
-                              //groupValues.add(value);
-                              //groupValues.add(value.toString());
-                              //groupValues.replaceRange(index, index, value);
-                              /* groupValues.replaceRange(index, index,
-                                  value.toString() as Iterable<String>); */
-                              /* groupValues.removeAt(index);
-                              groupValues.add(value); */
-                              //answers.add(snapshot.data![index]['Answer']);
-                              //groupValuesFixedLengthList[index] = value;
-                              /* groupValuesFixedLengthList[index] =
-                                  snapshot.data![index]['A']; */
-                              groupValuesFixedLengthList[index] =
-                                  value.toString();
-
-                              //value.toString();
-                              answersFixedLengthList[index] =
-                                  snapshot.data![index]['Answer'];
+                              groupVal[index] = value;
                             });
                           },
                         ),
@@ -126,20 +64,10 @@ class _QuestionScreenState extends State<QuestionScreen> {
                             snapshot.data![index]['B'],
                           ),
                           value: snapshot.data![index]['B'],
-                          groupValue: groupValues[index],
+                          groupValue: groupVal[index],
                           onChanged: (value) {
                             setState(() {
-                              //groupValues[index] = value.toString();
-                              //groupValues.add(value.toString());
-                              /* groupValues.replaceRange(index, index,
-                                  value.toString() as Iterable<String>); */
-                              /* groupValues.removeAt(index);
-                              groupValues.add(value); */
-                              //answers.add(snapshot.data![index]['Answer']);
-                              groupValuesFixedLengthList[index] =
-                                  value.toString();
-                              answersFixedLengthList[index] =
-                                  snapshot.data![index]['Answer'];
+                              groupVal[index] = value;
                             });
                           },
                         ),
@@ -148,21 +76,10 @@ class _QuestionScreenState extends State<QuestionScreen> {
                             snapshot.data![index]['C'],
                           ),
                           value: snapshot.data![index]['C'],
-                          groupValue: groupValues[index],
+                          groupValue: groupVal[index],
                           onChanged: (value) {
                             setState(() {
-                              //groupValues[index] = value.toString();
-                              //groupValues.add(value.toString());
-                              /* groupValues.replaceRange(index, index,
-                                  value.toString() as Iterable<String>); */
-
-                              /* groupValues.removeAt(index);
-                              groupValues.add(value); */
-                              //answers.add(snapshot.data![index]['Answer']);
-                              groupValuesFixedLengthList[index] =
-                                  value.toString();
-                              answersFixedLengthList[index] =
-                                  snapshot.data![index]['Answer'];
+                              groupVal[index] = value;
                             });
                           },
                         ),
@@ -171,21 +88,10 @@ class _QuestionScreenState extends State<QuestionScreen> {
                             snapshot.data![index]['D'],
                           ),
                           value: snapshot.data![index]['D'],
-                          groupValue: groupValues[index],
+                          groupValue: groupVal[index],
                           onChanged: (value) {
                             setState(() {
-                              //groupValues[index] = value.toString();
-                              //groupValues.add(value.toString());
-                              //groupValues.replaceRange(index, index,value.toString() as Iterable<String>);
-                              /* groupValues.removeAt(index);
-                              groupValues.add(value); */
-                              //answers.add(snapshot.data![index]['Answer']);
-                              groupValuesFixedLengthList[index] =
-                                  value.toString();
-                              answersFixedLengthList[index] =
-                                  snapshot.data![index]['Answer'];
-                              groupValues = groupValuesFixedLengthList;
-                              answers = answersFixedLengthList;
+                              groupVal[index] = value;
                             });
                           },
                         ),
@@ -200,58 +106,19 @@ class _QuestionScreenState extends State<QuestionScreen> {
       ),
       floatingActionButton: FloatingActionButton(
         tooltip: "Go to Home",
-        onPressed: () {
+        onPressed: () /* ß */ {
           Future<void> getQuestionsMapsList() async {
             String baseUrl = "http://127.0.0.1:8000/api/questions";
             var response = await http.get(Uri.parse(baseUrl));
-            /* QuestionModel questionModel = QuestionModel.fromJson(response.body);
-            print(questionModel); */
-            //print(response.body);
-            var resBody = response.body;
-            //print(resBody[3].toString());
-            //print(resBody[o]);  //[
-            //print(resBody[3]);  //i
-            //print(resBody[2]); //"
-            //print(resBody.length); //573
-            //print(resBody.length);
 
-            //Question questionModel =Question();
+            var resBody = response.body;
 
             Question questions = Question.fromJson(resBody);
-            print(questions);
-
-            print(groupValues);
 
             return;
           }
 
           getQuestionsMapsList();
-
-          double mark = 0.0;
-          //print(groupValues);
-          //print(answers);
-          //print(questionController.getAllQuestions());
-
-          /* for (int i = 0; i < 4 /* groupValues.length */; i++) {
-            /* print(groupValues[i]);
-            print(answers[i]);
-            print(groupValues);
-            print(answers); */
-            if (groupValues[i] == answers[i]) {
-              mark++;
-            } else {
-              mark = mark - 1 / 4;
-            }
-          } */
-          /* if (kDebugMode) {
-            print(mark);
-          }
-          print(mark.toString());
-          print(groupValues.toString());
-          print(answers); */
-          //print(groupValues);
-          //print(answers);
-          //Get.toNamed('/home');
         },
         child: const Icon(Icons.home_filled),
       ),
